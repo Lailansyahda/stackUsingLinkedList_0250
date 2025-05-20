bfdb991e862cfb152cb3db9e595683b6c497e44c
Lailansyahda <syahdaazalia123@gmail.com>
2025-05-20 10:31:58 +0700

    case 1 switch

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -88,7 +88,11 @@ int main()
 
         switch (choice)
         {
-            
+            case 1:
+                cout << "Enter the value to push : ";
+                    cin >> value;
+                stact.push(value);
+                break;
         }
     }
 }
