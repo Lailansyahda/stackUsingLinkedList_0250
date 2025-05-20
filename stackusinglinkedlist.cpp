a5d2c42142680de06626da86cac93d8fa04ce816
Lailansyahda <syahdaazalia123@gmail.com>
2025-05-20 10:20:24 +0700

    else void peek

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -50,7 +50,16 @@ class Stack
         {
             cout << "List is Empty." << endl;
         }
-       
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
     }   
 
 };
